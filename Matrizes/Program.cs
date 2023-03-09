e0685815050d2ac82766b6c67be2b850a465aec7
GBFerro <giovani.ferro@usp.br>
2023-03-08 22:05:27 -0300

    Add project files.

---
+++ Matrizes/Program.cs
@@ -0,0 +1,24 @@
+﻿int[,] mat = new int[5, 5];
+
+for (int linha = 0; linha < 5; linha++)
+{
+    for (int col = 0; col < 5; col++)
+    {
+        mat[linha, col] = linha * col;
+    }
+}
+
+for (int linha = 0; linha < 5; linha++)
+{
+    for (int col = 0; col < 5; col++)
+    {
+        Console.Write(mat[col, linha] + " ");
+    }
+    Console.WriteLine("\n");
+}
+
+for (int i = 0, j = 4; i < 5; i++)
+{
+    Console.Write(mat[i, j]+" ");
+    j--;
+}
